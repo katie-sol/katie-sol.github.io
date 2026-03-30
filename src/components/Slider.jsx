23f71bc7e116f994a794cae039f5aee84682167b
KatieSol <walker02@allegheny.edu>
2026-03-30 10:50:10 -0400

    add monday in 3/30/26 class items

---
+++ src/components/Slider.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+
+const Slider = ({slides}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const autoScroll = () => {
+        if (currentIndex === slides.length -1) {
+            return setCurrentIndex(0)
+        }
+        return setCurrentIndex(currentIndex + 1);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {autoScroll()}, 3000);
+        return () => clearInterval(interval);
+    })
+
+    return (
+        <div className = 'image-slider'>
+            <ul>
+                {slides.map((slide, index) => (
+                    <li key = {index} className = {index === currentIndex ? "active" : ""}>
+                        <img 
+                            src = {slide.src} 
+                            alt = {slide.alt} 
+                            style = {
+                                transform: `translateX(calc(-${currentIndex = 100}% - ${currentIndex * 3}rem))`
+                            }
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
