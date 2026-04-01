a6e3c5b655ab4751c187625efd075a4ed0cc8fd7
KatieSol <walker02@allegheny.edu>
2026-04-01 10:14:42 -0400

    started working on circle.jsx

---
+++ src/components/Circle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Circle = ({rating}) => {
+    const MAX_CIRCLES = 3;
+    let score = 0;
+
+    const text = String(rating).trim();
+
+    if (*.test(text)) {
+
+    }
+}
