a2fbbc4e0a0464c1d4871d34fe86fec14ca82cc6
KatieSol <walker02@allegheny.edu>
2026-04-03 10:43:25 -0400

    removed unused items and updated circle.jsx

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -7,6 +7,30 @@ const Circle = ({rating}) => {
     const text = String(rating).trim();
 
     if (*.test(text)) {
-
+        score = text.length;
     }
+    else if (!isNaN(parseFloat(text))) {
+        score = parseFloat(text);
+    } else {
+        return <p className = "skill-circles">{text}</p>
+    }
+
+    const fullCircles = Math.floor(score);
+    const hasHalfCircle = (score % 1) >= 0.5;
+    const emptyCircles = Math.mac(0, MAX_CIRCLES - fullCircles - (hasHalfCircle ? 1 : 0));
+
+    const fullChar = '●';
+    const halfChar = '◐';
+    const emptyChar = '○';
+
+    let circleString = '';
+    
+    for (let i = 0; i < fullCircles; i++)
+        circleString += fullChar;
+    if (hasHalfCircle)
+        circleString += halfChar;
+    for (let i = 0; i < emptyCircles; i++)
+        circleString += emptyChar;
+
+    return ;
 }
